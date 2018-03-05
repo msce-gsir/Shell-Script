5b419dfcfb9022630d4ee9a401c3747fcc79b105
msce-gsir <qingwei_gao@163.com>
2018-03-05 16:43:54 +0100

    calculate density

--- density_Z.sh
+++ density_Z.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cd 
+
+
+gmx density -f ./pfs/nobackup/home/q/qwgao/gmx/ChCl_Urea/contact_angle/0.0e/0e/300K/80ns/nvt.trr -s ./pfs/nobackup/home/q/qwgao/gmx/ChCl_Urea/contact_angle/0.0e/0e/300K/80ns/nvt.tpr -n ./pfs/nobackup/home/q/qwgao/gmx/ChCl_Urea/contact_angle/0.0e/0e/300K/index.ndx -b 70000 -e 80000 -d Z -dens number -sl 300 -o density_Z_0.0e.xvg 
+
+echo "3"
